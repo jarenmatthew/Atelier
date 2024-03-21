607f27041b6f19ac0270333f653acc281c020ca1
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2024-03-21 16:28:11 +0800

    Edited some wrong file path

--- Atelier/atelier-web/src/main.tsx
+++ Atelier/atelier-web/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LogInPage from "./Pages/Signup/Login.tsx";
+import LogInPage from "./Pages/Signup/LogIn.tsx";
 
 const router = createBrowserRouter([
   {
