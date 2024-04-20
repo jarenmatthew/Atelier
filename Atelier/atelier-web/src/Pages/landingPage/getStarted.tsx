8fbe822f182284a5f20363a18eda7387a778d48e
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2024-04-20 20:16:32 +0800

    updated landing page

--- Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
+++ Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
@@ -3,6 +3,7 @@ import Footer from '../../Footer';
 import { Box } from '@mui/material';
 import './getStarted.css';
 import { Link } from 'react-router-dom';
+import { Height } from '@mui/icons-material';
 
 
 
@@ -10,16 +11,31 @@ const GetStarted: React.FC = () => {
     
     return(
         <div>
-            <Header />
-            
             <Box>
-                <div className='first-sec'>
-                    <h3>Atelier</h3>
-                    <p></p>
+                <div className='sectionA'>
+                    <div className='sectionA-box1'>
+                        <div className='sectionA-box1A'>
+                        <img src='./src/assets/dummylogo.jpg' alt='Atelier Logo' id='logo'/>
+                        <h2 className='atelier-header'>Atelier</h2>
+                        </div>
+                    <div style={{height: '250px'}}></div>
+                    <h1 className='catch-phrase'>CATCH PHRASE</h1>
+                    <p className='atelier-desc'> 
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                    </p>
+                    <button className='get-started-button'>GET STARTED</button>
 
+                    </div>
+
+                    <div className='sectionA-box2'>
+                    <img src='./src/assets/dummylogo.jpg' alt='Atelier Logo' id='bigimg'/>
+
+
+                    </div>
                 </div>
-                <div className='second-sec'>
-
+                <div className='sectionB'>
                 </div>
             </Box>
 
