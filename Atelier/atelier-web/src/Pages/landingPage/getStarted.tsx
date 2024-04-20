1aa324c0908a9f1f5202723d82e85d92715234d3
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2024-04-20 22:15:30 +0800

    added new elements for landing page

--- Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
+++ Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
@@ -1,4 +1,3 @@
-import Header from '../../Header';
 import Footer from '../../Footer';
 import { Box } from '@mui/material';
 import './getStarted.css';
@@ -35,6 +34,54 @@ const GetStarted: React.FC = () => {
                 </div>
 
                 <div className='sectionB'>
+                    <div className='sectionB-box1'>
+                        <h1 id='howitworks-heading'>How it Works</h1>
+                    </div>
+                    <div style={{height: '50px'}}></div>
+                    
+                    <div className='sectionB-box2'>
+                        <div className='sectionB-box2A'>
+                        <img src='./src/assets/chat.jpg' alt='About Img' id='guideimg'/>
+                        </div>
+                        <div className='sectionB-box2B'>
+                        <h2>Directly Interact</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                        </p>
+                        </div>
+                    </div>
+                    <div style={{height: '50px'}}></div>
+                    
+                    <div className='sectionB-box2'>
+                        <div className='sectionB-box3A'>
+                        <h2>Follow fellow artists</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                        </p>
+                        </div>
+                        <div className='sectionB-box3B'>
+                        <img src='./src/assets/follow.jpg' alt='About Img' id='guideimg'/>
+                        </div>
+                    </div>
+                    <div style={{height: '50px'}}></div>
+                    
+                    <div className='sectionB-box2'>
+                        <div className='sectionB-box2A'>
+                        <img src='./src/assets/post.jpg' alt='About Img' id='guideimg'/>
+                        </div>
+                        <div className='sectionB-box2B'>
+                        <h2>Promote your art</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                        </p>
+                        </div>
+                    </div>
                 </div>
 
                 <div className='sectionC'>
