f4778198860ebce4a0d3ac96a58e1cc01073e919
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2024-04-21 02:55:06 +0800

    updated landing page ui

--- Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
+++ Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
@@ -11,11 +11,11 @@ const GetStarted: React.FC = () => {
     return(
         <div>
             <Box>
-                <div className='sectionA'>
-                    <div className='sectionA-box1'>
-                        <div className='sectionA-box1A'>
+                <div className='hero-section'>
+                    <div className='atelier-brand'>
+                        <div className='atelier-logo'>
                         <img src='./src/assets/dummylogo.jpg' alt='Atelier Logo' id='logo'/>
-                        <h2 className='atelier-header'>Atelier</h2>
+                        <h2 className='atelier-title'>Atelier</h2>
                         </div>
                     <div style={{height: '250px'}}></div>
                     <h1 className='catch-phrase'>CATCH PHRASE</h1>
@@ -25,25 +25,25 @@ const GetStarted: React.FC = () => {
                         ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
                     </p>
                     <div style={{height: '50px'}}></div>
-                    <button className='get-started-button'>Get Started</button>
+                    <button id='get-started-button'>Get Started</button>
                     </div>
 
-                    <div className='sectionA-box2'>
-                    <img src='./src/assets/dummylogo.jpg' alt='Atelier Logo' id='bigimg'/>
+                    <div className='hero-image'>
+                    <img src='./src/assets/atelier-room.png' alt='Atelier Room' id='hero'/>
                     </div>
                 </div>
+                <div style={{height: '50px'}}></div>
 
-                <div className='sectionB'>
-                    <div className='sectionB-box1'>
+                <div className='main-features-section'>
+                    <div className='features-heading'>
                         <h1 id='howitworks-heading'>How it Works</h1>
                     </div>
-                    <div style={{height: '50px'}}></div>
                     
-                    <div className='sectionB-box2'>
-                        <div className='sectionB-box2A'>
-                        <img src='./src/assets/chat.jpg' alt='About Img' id='guideimg'/>
+                    <div className='main-feature-box'>
+                        <div className='main-feature-img'>
+                        <img src='./src/assets/chat.png' alt='About Img' id='mfimg'/>
                         </div>
-                        <div className='sectionB-box2B'>
+                        <div className='main-feature-desc'>
                         <h2>Directly Interact</h2>
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
@@ -52,10 +52,9 @@ const GetStarted: React.FC = () => {
                         </p>
                         </div>
                     </div>
-                    <div style={{height: '50px'}}></div>
                     
-                    <div className='sectionB-box2'>
-                        <div className='sectionB-box3A'>
+                    <div className='main-feature-box'>
+                        <div className='main-feature-desc2'>
                         <h2>Follow fellow artists</h2>
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
@@ -63,17 +62,16 @@ const GetStarted: React.FC = () => {
                         ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
                         </p>
                         </div>
-                        <div className='sectionB-box3B'>
-                        <img src='./src/assets/follow.jpg' alt='About Img' id='guideimg'/>
+                        <div className='main-feature-img2'>
+                        <img src='./src/assets/follow.png' alt='About Img' id='mfimg'/>
                         </div>
                     </div>
-                    <div style={{height: '50px'}}></div>
                     
-                    <div className='sectionB-box2'>
-                        <div className='sectionB-box2A'>
-                        <img src='./src/assets/post.jpg' alt='About Img' id='guideimg'/>
+                    <div className='main-feature-box'>
+                        <div className='main-feature-img'>
+                        <img src='./src/assets/post.png' alt='About Img' id='mfimg'/>
                         </div>
-                        <div className='sectionB-box2B'>
+                        <div className='main-feature-desc'>
                         <h2>Promote your art</h2>
                         <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
@@ -84,10 +82,44 @@ const GetStarted: React.FC = () => {
                     </div>
                 </div>
 
-                <div className='sectionC'>
-                </div>
+                <div className='featured-arts-section'>
+                    <div className='features-heading'>
+                        <h1 id='artworks-heading'>Featured Artworks</h1>
+                    </div>
 
-                <div className='sectionD'>
+                    <div className='artworks-box'>
+                        <div className='artwork-img'>
+                            <img src="./src/assets/dummylogo.jpg" alt="Featured Artwork" id='artwork'/>
+                        </div>
+                        <div className='artwork-img'>
+                            <img src="./src/assets/dummylogo.jpg" alt="Featured Artwork" id='artwork'/>
+                        </div>
+                    </div>
+                </div>
+                <div style={{height: '50px'}}></div>
+
+                <div className='featured-artists-section'>
+                    <div className='features-heading'>
+                        <h1>Featured Artists</h1>
+                    </div>
+
+                    <div className='artists-box'>
+                        <div className='artist-profile'>
+                        <img src="./src/assets/dummylogo.jpg" alt="Artist Profile" id='artist'/>
+                        <h4>Name</h4>
+
+                        </div>
+                        <div className='artist-profile'>
+                        <img src="./src/assets/dummylogo.jpg" alt="Artist Profile" id='artist'/>
+                        <h4>Name</h4>
+
+                        </div>
+                        <div className='artist-profile'>
+                        <img src="./src/assets/dummylogo.jpg" alt="Artist Profile" id='artist'/>
+                        <h4>Name</h4>
+
+                        </div>
+                    </div>
                 </div>
             </Box>
 
