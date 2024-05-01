2b200a08168e97ba8089268790a4861600eafdf3
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2024-05-02 00:14:07 +0800

    separated user profile style css file

--- Atelier/atelier-web/src/Pages/Profile/userProfile.tsx
+++ Atelier/atelier-web/src/Pages/Profile/userProfile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { getDownloadURL, ref, listAll } from 'firebase/storage';
 import { storage } from '../../../FirebaseConfig';
-import './shopStyle.css';
+import './userProfileStyle.css';
 import Header from '../../Header';
 import Footer from '../../Footer';
 
