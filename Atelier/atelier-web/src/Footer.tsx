fe8dec5b013cecca76b1b145c98676bc8eff5ffe
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2024-05-02 23:48:31 +0800

    edited footer ui

--- Atelier/atelier-web/src/Footer.tsx
+++ Atelier/atelier-web/src/Footer.tsx
@@ -6,6 +6,7 @@ import { storage } from '../FirebaseConfig';
 import './FooterStyle.css';
 
 const Footer: React.FC = () => {
+  const [logoIconURL, setLogoIconURL] = useState('');
   const [twitterIconURL, setTwitterIconURL] = useState('');
   const [facebookIconURL, setFacebookIconURL] = useState('');
   const [messengerIconURL, setMessengerIconURL] = useState('');
@@ -19,11 +20,13 @@ const Footer: React.FC = () => {
     try {
       // Fetch icon URLs from Firebase Storage
       const iconsRef = ref(storage, 'icons');
-      const twitterURL = await getDownloadURL(ref(iconsRef, 'twitter.png'));
-      const facebookURL = await getDownloadURL(ref(iconsRef, 'facebook.png'));
-      const messengerURL = await getDownloadURL(ref(iconsRef, 'messenger.png'));
-      const instagramURL = await getDownloadURL(ref(iconsRef, 'instagram.png'));
+      const logoURL = await getDownloadURL(ref(iconsRef, 'atelier-logo.png'));
+      const twitterURL = await getDownloadURL(ref(iconsRef, 'twitter-icon.png'));
+      const facebookURL = await getDownloadURL(ref(iconsRef, 'facebook-icon.png'));
+      const messengerURL = await getDownloadURL(ref(iconsRef, 'messenger-icon.png'));
+      const instagramURL = await getDownloadURL(ref(iconsRef, 'insta-icon.png'));
       
+      setLogoIconURL(logoURL);
       setTwitterIconURL(twitterURL);
       setFacebookIconURL(facebookURL);
       setMessengerIconURL(messengerURL);
@@ -34,27 +37,44 @@ const Footer: React.FC = () => {
   };
 
   return (
-    <div>
-    <footer id="footer">
-      <div>
-        <h2>Atelier</h2>
-        <ul id="footer-navbar">
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Terms and Conditions</a></li>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">About Us</a></li>
-        </ul>
+    <div id="footer">
+
+      <footer id='footer-main'>
+
+        <div id='footer-head'>
+          <div><Link to="/"><img src={logoIconURL} className="logo" alt="Atelier Logo" /></Link></div>
+        </div>
+
+        <div id='footer-body'>
+
+          <div id="footer-navbar">
+
+            <div className='text-links'>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Terms and Conditions</a></li>
+            </div>
+            <div className='text-links'>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">About Us</a></li>
+            </div>
+
+          </div>
+
+          <div id="footer-icons">
+            <img src={twitterIconURL} alt="Twitter" className="socmed" />
+            <img src={facebookIconURL} alt="Facebook" className="socmed" />
+            <img src={messengerIconURL} alt="Messenger" className="socmed" />
+            <img src={instagramIconURL} alt="Instagram" className="socmed" />
+          </div>
+
+        </div>
+
+      </footer>
+
+      <div id='footer-end'>
+        <h4 id='copyright'>Copyright 2024</h4>
       </div>
-      <div className="socMedIcons">
-        <img src={twitterIconURL} alt="Twitter" className="socmed" />
-        <img src={facebookIconURL} alt="Facebook" className="socmed" />
-        <img src={messengerIconURL} alt="Messenger" className="socmed" />
-        <img src={instagramIconURL} alt="Instagram" className="socmed" />
-      </div>
-    </footer>
-    <div className="copyright">
-        <h3>Copyright 2024</h3>
-      </div>
+
     </div>
   );
 };
