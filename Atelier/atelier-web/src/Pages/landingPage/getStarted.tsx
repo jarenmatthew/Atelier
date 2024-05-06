ebbfe8ea078948ef4df7d0f4bb172f1daf58bb68
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2024-05-07 03:01:47 +0800

    edited landing/get started page ui

--- Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
+++ Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
@@ -5,6 +5,7 @@ import { Link, Route } from 'react-router-dom';
 import { Height } from '@mui/icons-material';
 import LogInPage from '../Login/LogIn';
 import { Link as RouterLink } from 'react-router-dom';
+import SignUpPage from '../Signup/SignUp';
 
 
 const GetStarted: React.FC = () => {
@@ -15,21 +16,22 @@ const GetStarted: React.FC = () => {
                 <div className='hero-section'>
                     <div className='atelier-brand'>
                         <div className='atelier-logo'>
-                        <img src='./src/assets/dummylogo.jpg' alt='Atelier Logo' id='logo'/>
-                        <h2 className='atelier-title'>Atelier</h2>
+                        <img src='./src/assets/atelier-logo.png' alt='Atelier Logo' id='logo'/>
                         
                     </div>
-                    <div style={{height: '250px'}}></div>
-                    <h1 className='catch-phrase'>CATCH PHRASE</h1>
+                    <div style={{height: '150px'}}></div>
+                    <h1 className='catch-phrase'>Discover. Create. Connect.</h1>
                     <p className='atelier-desc'> 
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                    Atelier is a vibrant community where artists of all kinds come together to showcase their work, find inspiration, and support one another.
+                    With Atelier, you can effortlessly share and sell your art while engaging with a diverse network of fellow creatives. Whether you're a painter, sculptor, photographer, 
+                    or any other type of artist, Atelier provides the platform for you to shine.
+                    Join us today and unlock a world of artistic possibilities!
                     </p>
                     <div style={{height: '50px'}}></div>
-                    <Link component={RouterLink} to="/signup" id='get-started-button'>
-                        Get Started
-                    </Link>
+                    
+                    <button id='get-started-button'>
+                    <Link id='start-link' to="/signup">Get Started</Link>
+                    </button>
                     </div>
 
                     <div className='hero-image'>
@@ -49,7 +51,7 @@ const GetStarted: React.FC = () => {
                         </div>
                         <div className='main-feature-desc'>
                         <h2>Directly Interact</h2>
-                        <p>
+                        <p className='atelier-desc'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
                         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
                         ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
@@ -60,7 +62,7 @@ const GetStarted: React.FC = () => {
                     <div className='main-feature-box'>
                         <div className='main-feature-desc2'>
                         <h2>Follow fellow artists</h2>
-                        <p>
+                        <p className='atelier-desc'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
                         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
                         ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
@@ -77,7 +79,7 @@ const GetStarted: React.FC = () => {
                         </div>
                         <div className='main-feature-desc'>
                         <h2>Promote your art</h2>
-                        <p>
+                        <p className='atelier-desc'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
                         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
                         ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
