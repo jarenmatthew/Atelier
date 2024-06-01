80c298a8e2d530d36074090445841698ad14198b
angelcustodio3 <112701307+angelcustodio3@users.noreply.github.com>
2024-06-01 23:54:30 +0800

    edit landing page

--- Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
+++ Atelier/atelier-web/src/Pages/landingPage/getStarted.tsx
@@ -16,13 +16,13 @@ const GetStarted: React.FC = () => {
                         
                     </div>
                     <div style={{height: '150px'}}></div>
-                    <h1 className='catch-phrase'>Discover. Create. Connect.</h1>
+                    <h1 className='catch-phrase'>Where art finds its voice. Where artists find their community.</h1>
                     <p className='atelier-desc'> 
                     Atelier is a vibrant community where artists of all kinds come together to showcase their work, find inspiration, and support one another.
                     With Atelier, you can effortlessly share and sell your art while engaging with a diverse network of fellow creatives. Whether you're a painter, sculptor, photographer, 
                     or any other type of artist, Atelier provides the platform for you to shine.
-                    Join us today and unlock a world of artistic possibilities!
                     </p>
+                    <p className='join-us'>Join us today and unlock a world of artistic possibilities!</p>
                     <div style={{height: '50px'}}></div>
                     
                     <Link id='start-link' to="/SignUp">
@@ -51,9 +51,9 @@ const GetStarted: React.FC = () => {
                         <div className='main-feature-desc'>
                         <h2>Directly Interact</h2>
                         <p className='atelier-desc'>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
+                        Atelier fosters a sense of community among fellow artists, enthusiasts, and buyers. 
+                        Through its feature of direct messaging among users, Atelier encourages engagement, dialogue, and collaboration which strengthen connections 
+                        and a supportive system for artistic growth.
                         </p>
                         </div>
                     </div>
